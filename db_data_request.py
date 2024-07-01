5745db07edd6c8425d36f531eaf7f0698b96df10
MarkData <54993785+markdrivendata@users.noreply.github.com>
2024-07-01 16:04:10 +0100

    Update in: - db_data_request.py

--- db_data_request.py
+++ db_data_request.py
@@ -4,15 +4,14 @@ from sshtunnel import SSHTunnelForwarder
 import pandas as pd
 from os.path import expanduser
 
-SSH_HOST = 'ca.b.7gra.us'
-SSH_USER = 'ubuntu'
-SSH_PORT = 22
-DB_HOST = '127.0.0.1'
-DB_PORT = 3306
-DB_USER = 'blipzi'
-DB_PASS = 'Rh*t7t969vCF#v9JuzVR'
-DB_NAME = 'blipzi_v1'
-SQL_PORT = 3306
+SSH_HOST= 'ca.x.7gra.us'
+SSH_USER= 'ubuntu'
+SSH_PORT= 22
+DB_HOST='127.0.0.1'
+SQL_PORT= 3306
+DB_USER= 'appgeek'
+DB_PASS= 'LY8Beg8ascQkXY'
+DB_NAME = 'appgeek'
 path = '/.ssh/id_rsa'
 
 home_dir = expanduser('~')
@@ -25,7 +24,6 @@ def db_data(key):
                             ssh_username=SSH_USER,
                             ssh_pkey=key,
                             remote_bind_address=(DB_HOST, SQL_PORT)) as tunnel:
-
         client = pymysql.connect(user=DB_USER,
                                  password=DB_PASS,
                                  host=DB_HOST,
@@ -36,49 +34,12 @@ def db_data(key):
         cursor = client.cursor()
         cursor.execute("""
        SELECT
-            YEAR(date_published) AS year,
-            MONTH(date_published) AS month,
-            COUNT(game_id) AS number_articles,
-            'blipzi.com' AS product,
-            'Game' AS content_type,
-            'New content' AS content_freshness
+      *
         FROM
-            games
-        WHERE
-            status LIKE 'online'
-        GROUP BY
-            YEAR(date_published),
-            MONTH(date_published)
-        UNION ALL
-        SELECT
-            YEAR(date_updated) AS year,
-            MONTH(date_updated) AS month,
-            COUNT(IF(DATEDIFF(date_updated, date_published) > 0, game_id, NULL)) AS number_articles,
-            'blipzi.com' AS product,
-            'Game' AS content_type,
-            'Update existing content' AS content_freshness
-        FROM
-            games
-        WHERE
-            status LIKE 'online'
-        GROUP BY
-            YEAR(date_updated),
-            MONTH(date_updated)
-        UNION ALL
-        SELECT
-            YEAR(date_created) AS year,
-            MONTH(date_created) AS month,
-            COUNT(id) AS number_articles,
-            'blipzi.com' AS product,
-            'Categories' AS content_type,
-            "" AS content_freshness
-        FROM
-            categories
-        WHERE
-            status LIKE 'online'
-        GROUP BY
-            YEAR(date_created),
-            MONTH(date_created)
+            articles
+  LIMIT
+  5
+
                 """)
         rows = cursor.fetchall()
         header = [i[0] for i in cursor.description]
@@ -92,5 +53,3 @@ def db_data(key):
 
 
 db_data(key_ssh)
-
-
